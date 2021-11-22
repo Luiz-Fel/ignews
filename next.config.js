0f316e21e6400a0143611088d39b1c21075fd94d
Luiz Felipe <lfsprj@gmail.com>
2021-11-22 20:01:37 -0300

    Added shop funcionality[WIP]

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+  }
 }
